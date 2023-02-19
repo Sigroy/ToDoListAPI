8f80176a26bc6b9a2ba8ab2513766ae094226e9e
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-18 18:09:02 -0600

    parse request uri and add rewrite rules

--- api/index.php
+++ api/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "Hello world";
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[3];
+
+$id = $parts[4] ?? null;
+
+echo $resource, ", ", $id;
+
+echo $_SERVER['REQUEST_METHOD'];
+
+if ($resource != 'tasks') {
+//    header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found", response_code: 404);
+    http_response_code(404);
+    exit;
+}
