04039888556faa606b310491c6700e44f1c4f164
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-18 22:00:12 -0600

    Create a method that processes requests

--- src/TaskController.php
+++ src/TaskController.php
@@ -1,8 +1,31 @@
 <?php
 
-namespace src;
+namespace ToDoListApi;
 
 class TaskController
 {
 
+    public function processRequest(string $method, ?string $id): void
+    {
+        if ($id === null || $id === '') {
+            if ($method === 'GET') {
+                echo 'index';
+            } elseif ($method === 'POST') {
+                echo 'create';
+            }
+        } else {
+            switch ($method) {
+                case 'GET':
+                    echo "show $id";
+                    break;
+                case 'PUT':
+                case 'PATCH':
+                    echo "update $id";
+                    break;
+                case 'DELETE':
+                    echo "delete $id";
+                    break;
+            }
+        }
+    }
 }
