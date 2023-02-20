9b3b12dbc5d93e1d81474ffd72254f73f4de4216
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-20 01:35:55 -0600

    create database class for database connection

--- src/Database.php
+++ src/Database.php
@@ -4,5 +4,17 @@ namespace ToDoListApi;
 
 class Database
 {
+    public function __construct(private string $host, private string $name, private string $user, private string $password)
+    {
 
+    }
+
+    public function getConnection(): \PDO
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8;port=3307";
+        $pdo = new \PDO($dsn, $this->user, $this->password, [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+        ]);
+        return $pdo;
+    }
 }
