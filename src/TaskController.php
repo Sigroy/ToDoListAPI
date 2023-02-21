c8638423083eddcf8150193a2fa21a1fd81e9776
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-21 13:24:01 -0600

    add code to create, update and delete resources

--- src/TaskController.php
+++ src/TaskController.php
@@ -15,7 +15,15 @@ class TaskController
             if ($method === 'GET') {
                 echo json_encode($this->gateway->getAll());
             } elseif ($method === 'POST') {
-                echo 'create';
+                $data = (array)json_decode(file_get_contents("php://input"), true);
+                $errors = $this->getValidationErrors($data);
+
+                if (!empty($errors)) {
+                    $this->respondUnprocessableEntity($errors);
+                    return;
+                }
+                $id = $this->gateway->create($data);
+                $this->respondCreated($id);
             } else {
                 $this->respondMethodNotAllowed('GET, POST');
             }
@@ -33,10 +41,19 @@ class TaskController
                     break;
                 case 'PUT':
                 case 'PATCH':
-                    echo "update $id";
+                    $data = (array)json_decode(file_get_contents("php://input"), true);
+                    $errors = $this->getValidationErrors($data, false);
+
+                    if (!empty($errors)) {
+                        $this->respondUnprocessableEntity($errors);
+                        return;
+                    }
+                    $rows = $this->gateway->update($id, $data);
+                    echo json_encode(["message" => "Task $id updated", "rows" => $rows]);
                     break;
                 case 'DELETE':
-                    echo "delete $id";
+                    $rows = $this->gateway->delete($id);
+                    echo json_encode(["message" => "Task $id deleted", "rows" => $rows]);
                     break;
                 default:
                     $this->respondMethodNotAllowed('GET, PUT, PATCH, DELETE');
@@ -55,5 +72,33 @@ class TaskController
     {
         http_response_code(404);
         echo json_encode(['message' => "Task with ID $id not found"]);
+    }
+
+    private function respondCreated(string $id): void
+    {
+        http_response_code(201);
+        echo json_encode(["message" => "Task created successfully", "id" => $id]);
+    }
+
+    private function respondUnprocessableEntity(array $errors): void
+    {
+        http_response_code(422);
+        echo json_encode(["errors" => $errors]);
+    }
+
+    private function getValidationErrors(array $data, bool $isNew = true): array
+    {
+        $errors = [];
+
+        if ($isNew && empty($data["name"])) {
+            $errors['name'] = "Required.";
+        }
+
+        if (!empty($data['priority'])) {
+            if (filter_var($data['priority'], FILTER_VALIDATE_INT) === false) {
+                $errors['priority'] = "Must be an integer.";
+            }
+        }
+        return $errors;
     }
 }
--- src/TaskGateway.php
+++ src/TaskGateway.php
@@ -41,4 +41,60 @@ class TaskGateway
         }
         return $data;
     }
+
+    public function create(array $data): string
+    {
+        $sql = "INSERT INTO task (name, priority, is_completed)
+                VALUES (:name, :priority, :is_completed)";
+        $statement = $this->conn->prepare($sql);
+        $statement->execute([$data['name'], empty($data['priority']) ? NULL : $data['priority'], $data['is_completed'] ?? false]);
+
+        return $this->conn->lastInsertId();
+    }
+
+    public function update(string $id, array $data): int
+    {
+        $fields = [];
+
+        if (!empty($data['name'])) {
+            $fields['name'] = $data['name'];
+
+        }
+
+        if (array_key_exists("priority", $data)) {
+            $fields['priority'] = $data['priority'];
+        }
+
+        if (array_key_exists("is_completed", $data)) {
+            $fields['is_completed'] = $data['is_completed'];
+        }
+
+        if (empty($fields)) {
+
+            return 0;
+        } else {
+
+            $sets = array_map(function ($value) {
+                return "$value = :$value";
+            }, array_keys($fields));
+
+            $sql = "UPDATE task"
+                . " SET " . implode(", ", $sets)
+                . " WHERE id = :id";
+
+            $fields['id'] = $id;
+            $statement = $this->conn->prepare($sql);
+            $statement->execute($fields);
+
+            return $statement->rowCount();
+        }
+    }
+
+    public function delete(string $id): int {
+        $sql = "DELETE FROM task
+                WHERE id = :id";
+        $statement = $this->conn->prepare($sql);
+        $statement->execute([$id]);
+        return $statement->rowCount();
+    }
 }
