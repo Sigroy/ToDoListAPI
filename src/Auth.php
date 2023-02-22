732b4f708a9d199d07eb77c1b6cbe57a77a68889
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-21 20:22:32 -0600

    Add authentication class to authenticate that the api key is sent in the request header and that exists in the database

---
+++ src/Auth.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ToDoListApi;
+
+use http\Client\Curl\User;
+
+class Auth
+{
+    public function __construct(private UserGateway $user_gateway)
+    {
+
+    }
+
+    public function authenticateAPIKey(): bool
+    {
+        if (empty($_SERVER['HTTP_X_API_KEY'])) {
+            http_response_code(400);
+            echo json_encode(['message' => 'Missing API key']);
+            return false;
+        }
+
+        $api_key = $_SERVER['HTTP_X_API_KEY'];
+
+        if ($this->user_gateway->getByAPIKey($api_key) === false) {
+            http_response_code(401);
+            echo json_encode(['message' => 'Invalid API key']);
+            return false;
+        }
+
+        return true;
+
+    }
+
+}
