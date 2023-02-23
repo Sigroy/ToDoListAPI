3aceac152896ac090e78d6bd5d397533048ece30
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-22 23:50:22 -0600

    Add DOCblocks for the methods

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -2,10 +2,18 @@
 
 namespace ToDoListApi;
 
+use ErrorException;
+use Throwable;
+
 class ErrorHandler
 {
-
-    public static function handleException(\Throwable $exception): void
+    /**
+     * Handle exceptions by logging the exception message, sending a 500 response code to the client
+     * and outputting a JSON response with information about the exception
+     * @param Throwable $exception
+     * @return void
+     */
+    public static function handleException(Throwable $exception): void
     {
         error_log($exception->getMessage());
         http_response_code(500);
@@ -18,11 +26,18 @@ class ErrorHandler
     }
 
     /**
-     * @throws \ErrorException
+     * Handle errors by converting them into exceptions using the ErrorException class and throwing them
+     * so that they can be handled by the handleException method
+     * @param int $severity
+     * @param string $message
+     * @param string $file
+     * @param int $line
+     * @return void
+     * @throws ErrorException
      */
     public static function handleError(int $severity, string $message, string $file, int $line): void
     {
-        throw new \ErrorException($message, 0, $severity, $file, $line);
+        throw new ErrorException($message, 0, $severity, $file, $line);
     }
 
 }
