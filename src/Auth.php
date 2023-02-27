344b0925bedf47046080eabd88b6e3cc8994aa47
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-27 16:08:38 -0600

    Add method to authenticate access token

--- src/Auth.php
+++ src/Auth.php
@@ -61,4 +61,33 @@ readonly class Auth
         return $this->user_id;
     }
 
+    public function authenticateAccessToken(): bool
+    {
+        if (!preg_match("/^Bearer\s+(.*)$/", $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
+            http_response_code(400);
+            echo json_encode(["message" => "Incomplete authorization header"]);
+            return false;
+        }
+
+        $plain_text = base64_decode($matches[1], true);
+
+        if ($plain_text === false) {
+            http_response_code(400);
+            echo json_encode(["message" => "Invalid authorization header"]);
+            return false;
+        }
+
+        $data = json_decode($plain_text, true);
+
+        if ($data === null) {
+            http_response_code(400);
+            echo json_encode(["message" => "Invalid JSON"]);
+            return false;
+        }
+
+        $this->user_id = $data['id'];
+
+        return true;
+    }
+
 }
