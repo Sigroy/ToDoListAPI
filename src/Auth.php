e7d74918405f72cfde7f64509b7c5732a5ecf6fd
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-28 16:05:44 -0600

    Add the JWTCodec as a dependency and decode the token sent to the server to see if it's valid

--- src/Auth.php
+++ src/Auth.php
@@ -16,7 +16,7 @@ readonly class Auth
      *
      * @param UserGateway $user_gateway The gateway to the user table to query the database
      */
-    public function __construct(private UserGateway $user_gateway)
+    public function __construct(private UserGateway $user_gateway, private JWTCodec $codec)
     {
 
     }
@@ -69,23 +69,20 @@ readonly class Auth
             return false;
         }
 
-        $plain_text = base64_decode($matches[1], true);
-
-        if ($plain_text === false) {
+        try {
+            $data = $this->codec->decode($matches[1]);
+        } catch (InvalidSignatureException) {
+            http_response_code(401);
+            echo json_encode(["message" => "Invalid signature"]);
+            return false;
+        } catch (\Exception $e) {
             http_response_code(400);
-            echo json_encode(["message" => "Invalid authorization header"]);
+            echo json_encode(["message" => $e->getMessage()]);
             return false;
         }
 
-        $data = json_decode($plain_text, true);
 
-        if ($data === null) {
-            http_response_code(400);
-            echo json_encode(["message" => "Invalid JSON"]);
-            return false;
-        }
-
-        $this->user_id = $data['id'];
+        $this->user_id = $data['sub'];
 
         return true;
     }
