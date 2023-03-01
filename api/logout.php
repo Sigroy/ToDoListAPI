3c479ebe9a63959603586255e568718c21c251ce
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-03-01 13:57:29 -0600

    Add endpoint to logout, request a new fresh token and to manage tokens

---
+++ api/logout.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/bootstrap.php';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    header("Allow: POST");
+    exit;
+}
+
+$data = (array)json_decode(file_get_contents("php://input"), true);
+
+if (!array_key_exists("token", $data)) {
+    http_response_code(400);
+    echo json_encode(["message" => "Missing token"]);
+    exit;
+}
+
+$codec = new \ToDoListApi\JWTCodec($_ENV["SECRET_KEY"]);
+
+try {
+    $payload = $codec->decode($data["token"]);
+} catch (Exception) {
+    http_response_code(400);
+    echo json_encode(["message" => "Invalid token"]);
+    exit;
+}
+
+$database = new \ToDoListApi\Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+
+$refresh_token_gateway = new \ToDoListApi\RefreshTokenGateway($database, $_ENV["SECRET_KEY"]);
+
+echo $refresh_token_gateway->delete($data["token"]);
+
---
+++ api/tokens.php
@@ -0,0 +1,20 @@
+<?php
+
+$payload = [
+    "sub" => $user["id"],
+    "name" => $user["name"],
+    "exp" => time() + 20,
+];
+$access_token = $codec->encode($payload);
+
+$refresh_token_expiry = time() + 432000;
+
+//$access_token = base64_encode(json_encode($payload));
+
+$refresh_token = $codec->encode([
+    "sub" => $user["id"],
+    "exp" => $refresh_token_expiry
+]);
+
+echo json_encode(["access_token" => $access_token,
+    "refresh_token" => $refresh_token]);
